1edc1b9bdb9e2b4d59295c88e4d53362fc6d33c9
Joanna Jones <jolees888@gmail.com>
2026-04-15 10:38:31 +0000

    Fix: explicitly serve sitemap.xml and robots.txt before catch-all route

--- server/static.ts
+++ server/static.ts
@@ -10,6 +10,17 @@ export function serveStatic(app: Express) {
     );
   }
 
+  // Explicitly serve SEO files before the catch-all route
+  app.get("/sitemap.xml", (_req, res) => {
+    res.setHeader("Content-Type", "application/xml");
+    res.sendFile(path.resolve(distPath, "sitemap.xml"));
+  });
+
+  app.get("/robots.txt", (_req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.sendFile(path.resolve(distPath, "robots.txt"));
+  });
+
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
